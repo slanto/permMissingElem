5ec2bd9d9d966c9865ce0229fd78c0d63781fae9
slanto <slawek.antos@outlook.com>
2017-01-07 02:10:29 +0100

    Get missing element implemented

---
+++ PermMissingElem/PermMissingElem.Core/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace PermMissingElem.Core
+{
+    public class Solution
+    {
+        public int  GetMissingElement(int[] a)
+        {
+            var maxLength = a.Length + a.Length + 1;
+            var result = 0;
+            for(int i = 0; i < maxLength; i++)
+            {
+                if (i < a.Length)
+                {
+                    result = result ^ a[i];
+                }
+                else
+                {
+                    result = result ^ i - (a.Length - 1);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ PermMissingElem/PermMissingElem.Tests/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using PermMissingElem.Core;
+using Xunit;
+
+namespace PermMissingElem.Tests
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 3, 1, 5 }, 4)]
+        [InlineData(new int[] { 2, 3 }, 1)]
+        public void GetMissingElement_ReturnsMissingElementForGivenArray(int[] array, int expected)
+        {
+            var sut = new Solution();
+            var actual = sut.GetMissingElement(array);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
